4f0d1f641e89a749214896a6cb9d603789658803
TracingInsights-Archive <153351807+TracingInsights-Archive@users.noreply.github.com>
2024-03-07 19:56:48 +0530

    Update tel.py

--- tel.py
+++ tel.py
@@ -18,8 +18,9 @@ def events_available(year: int) -> any:
 
 
 events = [
-    "Bahrain Grand Prix",
-    # 'Bahrain Grand Prix', 'Saudi Arabian Grand Prix',
+ 
+    # 'Bahrain Grand Prix', 
+    'Saudi Arabian Grand Prix',
     #     'Australian Grand Prix',
     # 'Azerbaijan Grand Prix',
     # 'Miami Grand Prix',
@@ -36,7 +37,7 @@ events = [
     # 'Las Vegas Grand Prix', 'Abu Dhabi Grand Prix',     'Japanese Grand Prix',
 ]
 sessions = [
-    "Race",
+    "Practice 1",
 ]
 
 
@@ -506,3 +507,50 @@ for event in events_list:
                 json.dump(laptimes, json_file)
 
             # print(f"Dictionary saved to {file_path}")
+
+
+
+
+
+
+
+
+# corners
+
+import fastf1
+import os
+import json
+import utils
+
+
+
+
+
+def sessions_available(year: int, event: str | int) -> any:
+    # get sessions available for a given year and event
+    event = str(event)
+    data = utils.LatestData(year)
+    sessions = data.get_sessions(event)
+    return sessions
+
+for event in events:
+    
+    for session in sessions:
+        f1session = fastf1.get_session(YEAR, event, session)
+        f1session.load()
+        circuit_info = f1session.get_circuit_info().corners
+        corner_info ={
+            "CornerNumber": circuit_info['Number'].tolist(),
+            "X": circuit_info['X'].tolist(),
+            "Y": circuit_info['Y'].tolist(),
+            "Angle": circuit_info['Angle'].tolist(),
+            "Distance": circuit_info['Distance'].tolist(),
+        }
+
+        driver_folder = f"{event}/{session}"
+        file_path = f"{event}/{session}/corners.json"
+        if not os.path.exists(driver_folder):
+            os.makedirs(driver_folder)
+        # Save the dictionary to a JSON file
+        with open(file_path, "w") as json_file:
+            json.dump(corner_info, json_file)
