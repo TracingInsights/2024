99ee09beeffc5cb9d8b8ce8f1f1d187a6e7ce371
TracingInsights-Archive <153351807+TracingInsights-Archive@users.noreply.github.com>
2024-04-19 11:33:45 +0530

    Update tel.py

--- tel.py
+++ tel.py
@@ -39,11 +39,12 @@ events = [
     'Chinese Grand Prix',
 ]
 sessions = [
-    "Practice 1",
+    # "Practice 1",
       # "Practice 2",
       # "Practice 3",
       # "Qualifying",
        # "Race",
+    "Sprint Qualifying",
 ]
 
 
