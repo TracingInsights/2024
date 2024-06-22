a228f0321c50b9c64952d8162178c115da21fe7c
TracingInsights-Archive <153351807+TracingInsights-Archive@users.noreply.github.com>
2024-06-22 20:38:32 +0530

    Update tel.py

--- tel.py
+++ tel.py
@@ -43,8 +43,8 @@ events = [
 sessions = [
     # "Practice 1",
       # "Practice 2",
-      "Practice 3",
-      # "Qualifying",
+     # "Practice 3",
+      "Qualifying",
        # "Race",
     # "Sprint",
      # "Sprint Qualifying",
