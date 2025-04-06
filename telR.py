58a00f686c124438ee763b2af873c6e6ad459fe9
TracingInsights-Archive <153351807+TracingInsights-Archive@users.noreply.github.com>
2025-04-06 20:56:12 +0530

    Update telR.py

--- telR.py
+++ telR.py
@@ -25,7 +25,7 @@ logging.getLogger("fastf1").propagate = False
 # Enable caching
 fastf1.Cache.enable_cache("cache")
 
-DEFAULT_YEAR = 2025
+DEFAULT_YEAR = 2024
 PROTO = "https"
 HOST = "api.multiviewer.app"
 HEADERS = {"User-Agent": f"FastF1/"}
